28d668b970ca867b64ba0fde534bea903ca2a742
Marvin Bredal Lillehaug <marvin.lillehaug@kantega.no>
2017-12-10 11:29:46 +0100

    close nav on anchor click

--- src/js/menu_button.js
+++ src/js/menu_button.js
@@ -1,9 +1,14 @@
 
 export default function registerMenuToggler() {
     const menuToggler = document.getElementById('menuToggler');
-    const nav = document.getElementsByTagName('nav')[0];
-    menuToggler.onclick = e => {
+    const nav = document.getElementsByClassName('c-navigation')[0];
+    let toggle = e => {
         menuToggler.classList.toggle('show--menu');
         nav.classList.toggle('show--menu');
     };
+    menuToggler.onclick = toggle;
+    const anchors = nav.getElementsByTagName('a');
+    for(let a of anchors) {
+        a.onclick = toggle;
+    }
 }
