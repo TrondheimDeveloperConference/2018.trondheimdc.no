3b16481c15acdd6b6ce80213625f06c2be21af06
Marvin Bredal Lillehaug <marvin.lillehaug@kantega.no>
2018-09-23 13:20:07 +0200

    Polyfills

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
+import polyfill from 'babel-polyfill';
+import fetch_polyfill from 'whatwg-fetch';
+
 import flickrImages from "./flickr";
 import registerLogoScrollListener from './logo';
 import registerMenuToggler from './menu_button';
